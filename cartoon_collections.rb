ec072f6324a470c0ec9a8a0c8fdf9f7169072911
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 23:35:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def long_planeteer_calls(array)
   if array.any? {|word| word.length > 4}
-  else array.all? |word| {word.length <= 4}
+  else array.all? {|word| word.length <= 4}
   end 
 end
 
